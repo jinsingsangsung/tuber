a098f14eb103bc9468bdfba5f5afdb8d054e1e2d
jinsung <nsml@node0.run-ri6ze4nn01lmre71430.nsml-nodes.svc.cluster.local>
2023-01-11 02:44:19 +0000

    additional edit

--- nsml_setup.sh
+++ nsml_setup.sh
@@ -5,13 +5,13 @@ cp -r /mnt/video-nfs5/datasets/hmdb51.tar /mnt/tmp # copy the dataset
 cd /mnt/tmp && tar -xvf JHMDB.tar && rm -rf JHMDB.tar # untar the dataset
 
 # if needed
-# cd /home/nsml/pretrained_models/tubelet-transformer/transformer
+# cd /home/nsml/pretrained_models/transformer
 # wget https://yzaws-data-log.s3.amazonaws.com/shared/TubeR_cvpr22/detr.pth
-# cd /home/nsml/pretrained_models/tubelet-transformer/backbone
+# cd /home/nsml/pretrained_models/backbone
 # wget https://yzaws-data-log.s3.amazonaws.com/shared/TubeR_cvpr22/irCSN_50_ft_kinetics_from_ig65m_f233743920.mat
-# cd /home/nsml/pretrained_models/tubelet-transformer/main
+# cd /home/nsml/pretrained_models/main
 # wget https://yzaws-data-log.s3.amazonaws.com/shared/TubeR_cvpr22/TubeR_CSN152_JHMDB.pth # case of JMDB
 
-cd /home/nsml/tubelet-trasnformer/datasets
+cd /home/nsml/tubelet-transformer/datasets
 wget https://yzaws-data-log.s3.amazonaws.com/shared/TubeR_cvpr22/assets.zip
 unzip assets.zip && rm -rf assets.zip
