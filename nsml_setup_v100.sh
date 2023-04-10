de502a32258f7962e56ee05677208ddb58dc5659
Jinsung Lee <76904126+jinsingsangsung@users.noreply.github.com>
2023-04-10 15:35:30 +0900

    Update nsml_setup_v100.sh

--- nsml_setup_v100.sh
+++ nsml_setup_v100.sh
@@ -4,3 +4,4 @@ cd /mnt/tmp
 tar -xf frames.tar
 rm -rf frames.tar
 cd /home/nsml/tuber
+cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-tuber-detr.pth ../pretrained_models/transformer/
