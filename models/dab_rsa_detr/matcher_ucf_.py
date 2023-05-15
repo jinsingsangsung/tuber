7dc21282a6260d99fbe43c136bb0df99ba1075da
jinsung <jinsunglee@postech.ac.kr>
2023-05-15 11:08:22 +0000

    matcher of rsa is fixed

--- models/dab_rsa_detr/matcher_ucf_.py
+++ models/dab_rsa_detr/matcher_ucf_.py
@@ -58,77 +58,87 @@ class HungarianMatcher(nn.Module):
                 len(index_i) = len(index_j) = min(num_queries, num_target_boxes)
         """
         l = self.clip_len
-        bs, t, num_queries = outputs["pred_logits"].shape[0:3]
+        bs, t, num_queries, num_classes = outputs["pred_logits"].shape
         num_classes = outputs["pred_logits"].shape[-1]
-        out_bbox = outputs["pred_boxes"].permute(0,2,1,3).flatten(0, 1)    # bs*nq, t, 4
+        out_bbox = outputs["pred_boxes"].permute(1,0,2,3).flatten(1, 2)    # t, bs*nq, 4
+        # out_prob = outputs["pred_logits"].permute(1,0,2,3).flatten(1, 2).softmax(-1) # t, bs*nq, num_classes
         # Also concat the target labels
         # tgt_bbox = torch.cat([v["boxes"] for v in targets]) 
-        boxes_per_batch = []
+        sizes = []
         tgt_bbox = []
-        if torch.cat([v["boxes"] for v in targets]).__len__() > l:
-            import pdb; pdb.set_trace()
+        # if torch.cat([v["boxes"] for v in targets]).__len__() > l:
+        #     import pdb; pdb.set_trace()
         for v in targets:
             tgt_bbox_ = v["boxes"]
             num_tubes = len(tgt_bbox_) // l
-            boxes_per_batch.append(num_tubes)
+            sizes.append(num_tubes)
             # tgt_bboxes = tgt_bbox_.split(l)
             # tgt_bbox.append(torch.stack([tgt_bboxes[i] for i in range(num_tubes)], dim=0)) # num_tubes x clip_len x 5
             tgt_bbox.append(tgt_bbox_.reshape(num_tubes, -1 ,5))
         tgt_bbox = torch.cat(tgt_bbox, dim=0)[..., 1:]
         tgt_ids = torch.cat([v["labels"] for v in targets])
-
+        invalid_ids = tgt_ids >= num_classes
         assert len(tgt_bbox) == len(tgt_ids)
         # TODO: Matching strategy: tube? or frame? let's go with tube
         # 1. make it generalizable to multiple actors, make a good output of matching indices (take the idea from ava)
         # 2. discard the outputs with non-meaningful labels (actually null tube) how to return the idx?
 
-        import pdb; pdb.set_trace()
-
-        # iou3d = []
-        pad = torch.zeros((len(tgt_bbox), 1), device=tgt_bbox.device)
-        
-        # _tgt_bbox = torch.cat([pad, box_cxcywh_to_xyxy(tgt_bbox)], -1)
-        # _out_bbox = torch.cat([pad[:, None].repeat(1, out_bbox.size(1), 1), box_cxcywh_to_xyxy(out_bbox)], -1)
-        # for n in range(out_bbox.size(1)):
-            # iou3d.append(compute_video_map.iou3d_voc(_out_bbox[:, n, :].detach().cpu().numpy(), _tgt_bbox.detach().cpu().numpy()))
-        # iou3d = torch.tensor(iou3d, device=tgt_bbox.device)[:, None] # n_q, 1 #3d iou
-
+        tgt_bbox = tgt_bbox.permute(1,0,2).contiguous()
+        tgt_ids = tgt_ids.permute(1,0).contiguous()
+        invalid_ids = invalid_ids.permute(1,0).contiguous() # clip_len x bs~#
         # Compute the L1 cost between boxes
-        cost_bbox = torch.cdist(out_bbox.reshape(bs*t, num_queries, 4), tgt_bbox[:, None], p=1).reshape(bs, t, num_queries, 1)
-        # bs, t, nq, 1
-        
+        cost_bbox = torch.cdist(tgt_bbox, out_bbox) # clip_len x bs~# x bs*n_q
+        cost_bbox[invalid_ids] = torch.zeros(out_bbox.shape[1], device=cost_bbox.device)
+        cost_bbox = cost_bbox.permute(0,2,1).contiguous()
+
         # Compute the giou cost betwen boxes
-        cost_giou = -batched_generalized_box_iou(box_cxcywh_to_xyxy(out_bbox).reshape(-1, num_queries, 4), box_cxcywh_to_xyxy(tgt_bbox).reshape(-1, 1, 4)).reshape(bs, t, num_queries, 1)
-        # bs, t, nq, 1
+        cost_giou = -batched_generalized_box_iou(box_cxcywh_to_xyxy(tgt_bbox), box_cxcywh_to_xyxy(out_bbox))
+        cost_giou[invalid_ids] = torch.zeros(out_bbox.shape[1], device=cost_bbox.device)
+        cost_giou = cost_giou.permute(0,2,1).contiguous()
+        out_prob = outputs["pred_logits_b"].permute(1,0,2,3).flatten(1, 2).softmax(-1)
+        cost_class = -out_prob[..., 1:2].repeat(1,1,sum(sizes))
+        # t, bs*n_q, bs~#
 
-        # out_prob = outputs["pred_logits"].flatten(0, 1).softmax(-1)
-        # bs*t, nq, num_classes
-        
-
-        # print(outputs["pred_logits_b"][..., 1:2].shape, bs, t)
-        # cost_class = -out_prob[..., tgt_ids[0]].reshape(bs, t, num_queries, 1)# * outputs["pred_logits_b"].softmax(-1)[..., 1:2]
-        # bs, t, nq, 1
-
-        out_prob_b = outputs["pred_logits_b"].softmax(-1)
-        cost_class = -out_prob_b[..., 1:2]
         # Final cost matrix
-        if not self.binary_loss:
-            cost_class_b = 0
-        C = self.cost_bbox * cost_bbox + self.cost_class * cost_class + self.cost_giou * cost_giou # + cost_class_b
+        C = self.cost_bbox * cost_bbox + self.cost_giou * cost_giou + self.cost_class * cost_class
+        # t, bs*n_q, bs~#
+
         # bs, t, num_queries, len(tgt_bbox)
-        C = C.view(bs*t, num_queries, -1).cpu()
-        # import pdb; pdb.set_trace()
-        # sizes = [len(v["boxes"]) for v in targets]
+        C = C.view(t, bs, num_queries, -1).cpu()
 
         indices = []
-        for i in range(bs):
-            for j in range(t):
-                indices.append(linear_sum_assignment(C[i*t+j]))
-        lst = [int(j) for (j,k) in indices]
-        idx = max(set(lst), key=lst.count)
-        # len(indices): bs * t
-        # each element: b번째 batch의 t번째 frame에 해당하는 GT에는 indices[b*T+t] 번째 query가 matching됨
-        # element may look like, (array([3]), array([0])): GT box는 하나이기 때문에 두 번째 원소는 항상 array([0])임.
+        output = []
+        
+        # matched_queries = []
+        for j in range(t):
+            idx = [linear_sum_assignment(c[i]) for i, c in enumerate(C[j].split(sizes, -1))]
+
+            for batch_id, (queries, keys) in enumerate(idx):
+                if len(output) < len(idx):
+                    interm = {}
+                    for q, k in zip(queries, keys):
+                        if interm.get(k) is None:
+                            interm[k] = []
+                        interm[k].append(q)
+                    output.append(interm) 
+                else:
+                    for q, k in zip(queries, keys):
+                        output[batch_id][k].append(q)
+            indices.append(idx) # idx length: batch
+        output_real = []
+        for batch_id, out_dict in enumerate(output):
+            interm = []
+            for k in out_dict:
+                interm.append(torch.tensor([max(set(out_dict[k]), key=out_dict[k].count), k]))
+            interm = torch.stack(interm).transpose(0, 1).contiguous()
+            output_real.append(interm)
+                
+
+        # if (torch.tensor(sizes) > 1).any():
+        #     print(output_real)
+
+        # t, num_
+        # element may look like, (array([3, 5]), array([1, 0])) # 1번 target은 3, 0번 target은 5번 query matched
 
         # indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
         # res = []
@@ -136,8 +146,8 @@ class HungarianMatcher(nn.Module):
         #     res.append((torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)))
         #
         # return res
-        return [(torch.as_tensor([idx], dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
-
+        # return [(torch.as_tensor([idx], dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
+        return output_real
 
 def build_matcher(cfg):
     return HungarianMatcher(cost_class=cfg.CONFIG.MATCHER.COST_CLASS, cost_bbox=cfg.CONFIG.MATCHER.COST_BBOX, cost_giou=cfg.CONFIG.MATCHER.COST_GIOU, data_file=cfg.CONFIG.DATA.DATASET_NAME, binary_loss=cfg.CONFIG.MATCHER.BNY_LOSS, before=cfg.CONFIG.MATCHER.BEFORE, clip_len=cfg.CONFIG.DATA.TEMP_LEN)
