6220c27371991f4998bcc7413f9e10f180b9b109
jinsung <jinsunglee@postech.ac.kr>
2023-05-17 07:06:06 +0000

    sigmoid focal loss edit

--- models/dab_new_detr/segmentation.py
+++ models/dab_new_detr/segmentation.py
@@ -197,7 +197,7 @@ def dice_loss(inputs, targets, num_boxes):
     return loss.sum() / num_boxes
 
 
-def sigmoid_focal_loss(inputs, targets, num_boxes, alpha: float = 0.25, gamma: float = 2):
+def sigmoid_focal_loss(inputs, targets, weights=None, alpha: float = 0.25, gamma: float = 2):
     """
     Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
     Args:
@@ -215,14 +215,15 @@ def sigmoid_focal_loss(inputs, targets, num_boxes, alpha: float = 0.25, gamma: f
     """
     prob = inputs.sigmoid()
     ce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
+    if weights is not None:
+        ce_loss *= weights
     p_t = prob * targets + (1 - prob) * (1 - targets)
     loss = ce_loss * ((1 - p_t) ** gamma)
 
     if alpha >= 0:
         alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
         loss = alpha_t * loss
-
-    return loss.mean(1).sum() / num_boxes
+    return loss.mean(-1).sum()
 
 
 class PostProcessSegm(nn.Module):
