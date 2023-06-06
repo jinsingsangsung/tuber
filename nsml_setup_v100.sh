8974cba7d4bde35d1b17e37c2304e0ef8739e2f7
jinsung <jinsunglee@postech.ac.kr>
2023-06-06 17:51:39 +0000

    nsml setup file updated

--- nsml_setup_v100.sh
+++ nsml_setup_v100.sh
@@ -11,8 +11,9 @@ rm -rf JHMDB.tar
 tar -xf UCF101_v2.tar.gz.tar
 rm -rf UCF101_v2.tar.gz.tar
 cd /home/nsml/tuber
-cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-tuber-detr.pth ../pretrained_models/transformer/
+python3 json_inspector.py
 cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-hier-detr.pth ../pretrained_models/transformer/
 cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/og-dab-detr.pth ../pretrained_models/transformer/
 cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-og-dab-detr.pth ../pretrained_models/transformer/
 cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-dab-hier-detr.pth ../pretrained_models/transformer/
+cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-sa-hier-detr.pth ../pretrained_models/transformer/
