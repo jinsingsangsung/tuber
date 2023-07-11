01ab0a136a56329e4ac2d2d59c03aa2f444e2540
jinsung <jinsunglee@postech.ac.kr>
2023-07-11 23:50:59 +0000

    ./nsml_setup_a100.sh

--- nsml_setup_a100.sh
+++ nsml_setup_a100.sh
@@ -7,8 +7,8 @@ cd /home/nsml/tuber
 python3 json_inspector.py
 # cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-tuber-detr.pth ../pretrained_models/transformer/
 cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-hier-detr.pth ../pretrained_models/transformer/
-cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/og-dab-detr.pth ../pretrained_models/transformer/
-cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-og-dab-detr.pth ../pretrained_models/transformer/
-cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-dab-hier-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/og-dab-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-og-dab-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-dab-hier-detr.pth ../pretrained_models/transformer/
 cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-sa-hier-detr.pth ../pretrained_models/transformer/
 cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-sa-hoper-rmbin.pth ../pretrained_models/transformer/
