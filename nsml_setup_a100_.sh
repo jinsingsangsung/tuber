5e28630a2373a1115d13540b07cd5aa1498096b8
jinsung <jinsunglee@postech.ac.kr>
2023-07-12 10:19:48 +0000

    new a100 setup file added

---
+++ nsml_setup_a100_.sh
@@ -0,0 +1,14 @@
+cd /mnt/video-nfs5/datasets/ava
+cp -r frames.tar /mnt/tmp
+cd /mnt/tmp
+tar -xf frames.tar
+rm -rf frames.tar
+cd /home/nsml/tuber
+python3 json_inspector.py
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-hier-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/og-dab-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-og-dab-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-dab-hier-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-sa-hier-detr.pth ../pretrained_models/transformer/
+cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-hoper-detr.pth ../pretrained_models/transformer/
+cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-deformable-detr.pth ../pretrained_models/transformer/
