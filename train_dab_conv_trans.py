enable baseline model gradient ckpt option as well, batch size setup error fixed

train_dab_conv_trans.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true', help="debug, and ddp is disabled")
     parser.add_argument('--eff', action='store_true', help="only for AVA, efficiently output only keyframe")
     parser.add_argument('--grad_ckpt', action='store_true', help="use gradient checkpoint")
-    parser.add_argument('--batch', default=4, type=int, help="use gradient checkpoint")
+    parser.add_argument('--batch', default=0, type=int, help="batch size")
     args = parser.parse_args()
     random.seed(args.random_seed)
     np.random.seed(args.random_seed)
@@ -169,7 +169,7 @@ if __name__ == '__main__':
         cfg.CONFIG.EFFICIENT = False
     if args.grad_ckpt:
         cfg.CONFIG.GRADIENT_CHECKPOINTING = True
-    if args.batch != 4:
+    if args.batch != 0:
         cfg.CONFIG.TRAIN.BATCH_SIZE = args.batch
     
     import socket 