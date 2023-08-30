e620233ad13a0f9b398c92a4b87bdf6a56cbc4f6
Jinsung Lee <76904126+jinsingsangsung@users.noreply.github.com>
2023-08-30 21:23:13 +0900

    Update nsml_setup_a100_.sh

--- nsml_setup_a100_.sh
+++ nsml_setup_a100_.sh
@@ -10,5 +10,5 @@ python3 json_inspector.py
 # cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-og-dab-detr.pth ../pretrained_models/transformer/
 # cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/finetuned-dab-hier-detr.pth ../pretrained_models/transformer/
 # cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-sa-hier-detr.pth ../pretrained_models/transformer/
-cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-hoper-detr.pth ../pretrained_models/transformer/
-cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-deformable-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-hoper-detr.pth ../pretrained_models/transformer/
+# cp -r /mnt/video-nfs5/users/jinsung/results/tubelet-transformer/dab-deformable-detr.pth ../pretrained_models/transformer/
