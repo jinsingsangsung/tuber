69724119814afdc1ba22dc41b9984867529cae02
jinsung <jinsunglee@postech.ac.kr>
2023-09-01 14:53:42 +0000

    edit: actor feature summation

--- models/dab_conv_trans_detr/dab_transformer.py
+++ models/dab_conv_trans_detr/dab_transformer.py
@@ -219,8 +219,9 @@ class TransformerDecoder(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.conv_activation = _get_activation_fn("gelu")
 
-        self.conv1 = nn.Conv2d(2*d_model, d_model, kernel_size=1)
-        self.conv2 = nn.Conv2d(d_model, d_model, kernel_size=1)
+        # self.conv1 = nn.Conv2d(2*d_model, d_model, kernel_size=1)
+        # self.conv2 = nn.Conv2d(d_model, d_model, kernel_size=1)
+        self.conv_norm = nn.LayerNorm(d_model)
         self.q_proj = nn.Conv2d(d_model, d_model, kernel_size=1)
         self.k_proj = nn.Conv2d(d_model, d_model, kernel_size=1)
         self.v_proj = nn.Conv2d(d_model, d_model, kernel_size=1)
@@ -299,9 +300,11 @@ class TransformerDecoder(nn.Module):
             actor_feature_expanded = actor_feature.flatten(0,1)[..., None, None].expand(-1, -1, h, w) # N_q*B, D, H, W
             encoded_feature_expanded = memory[:, None].expand(-1, len(tgt), -1, -1).flatten(1,2).view(h,w,-1,actor_feature.shape[-1]).permute(2,3,0,1) # N_q*B, D, H, W
 
-            cls_feature = self.conv_activation(self.conv1(torch.cat([actor_feature_expanded, encoded_feature_expanded], dim=1)))
-            cls_feature = self.conv_activation(self.conv2(cls_feature))
-            # cls_feature = self.bn1(cls_feature)
+            cls_feature = actor_feature_expanded + encoded_feature_expanded
+            cls_feature = self.conv_norm(cls_feature.transpose(-1, 1).contiguous()).transpose(-1, 1).contiguous()
+            # cls_feature = self.conv_activation(self.conv1(torch.cat([actor_feature_expanded, encoded_feature_expanded], dim=1)))
+            # cls_feature = self.conv_activation(self.conv2(cls_feature))
+
             query = self.q_proj(cls_feature)
             query = query[:, None].expand(-1, 80, -1, -1, -1)
             key = self.class_queries[None, :, :, None, None].expand(actor_feature_expanded.shape[0], -1, -1, h, w)
