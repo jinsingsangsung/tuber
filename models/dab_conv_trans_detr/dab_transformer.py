fix: gradient checkpoint option added

models/dab_conv_trans_detr/dab_transformer.py
@@ -20,6 +20,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn, Tensor
 from .attention import MultiheadAttention
+import torch.utils.checkpoint as checkpoint
 
 class MLP(nn.Module):
     """ Very simple multi-layer perceptron (also called FFN)"""
@@ -73,6 +74,7 @@ class Transformer(nn.Module):
                  num_patterns=0,
                  modulate_hw_attn=True,
                  bbox_embed_diff_each_layer=False,
+                 gradient_checkpointing=False,
                  ):
 
         super().__init__()
@@ -104,6 +106,8 @@ class Transformer(nn.Module):
             self.num_patterns = 0
         if self.num_patterns > 0:
             self.patterns = nn.Embedding(self.num_patterns, d_model)
+        
+        self.gradient_checkpointing = gradient_checkpointing            
 
     def _reset_parameters(self):
         for p in self.parameters():
@@ -122,7 +126,16 @@ class Transformer(nn.Module):
         refpoint_embed = refpoint_embed.repeat(1, bs, 1) #n_q, bs * t, 4
         mask = mask.flatten(0,1).flatten(1)
 
-        memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed, src_shape=src_shape) 
+        if self.gradient_checkpointing:
+            def custom_encoder(module, mask, pos_embed, src_shape):
+                def custom_forward(inputs):
+                    inputs = module(inputs, src_key_padding_mask=mask, pos=pos_embed, src_shape=src_shape)
+                    return inputs
+                return custom_forward
+            memory = checkpoint.checkpoint(custom_encoder(self.encoder, mask, pos_embed, src_shape), src)
+            
+        else:
+            memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed, src_shape=src_shape)
         # temporal dimension is alive
         # query_embed = gen_sineembed_for_position(refpoint_embed)
         num_queries = refpoint_embed.shape[0]
@@ -137,8 +150,17 @@ class Transformer(nn.Module):
         # tgt = self.patterns.weight[:, None, None, :].repeat(1, self.num_queries, bs*t, 1).flatten(0, 1) # n_q*n_pat, bs, d_model
         # refpoint_embed = refpoint_embed.repeat(self.num_patterns, 1, 1) # n_pat*n_q, bs*t, d_model
             # import ipdb; ipdb.set_trace()
-        hs, cls_hs, references = self.decoder(tgt, memory, memory_key_padding_mask=mask,
-                          pos=pos_embed, refpoints_unsigmoid=refpoint_embed, orig_res=(h,w))
+        if self.gradient_checkpointing:
+            def custom_decoder(module, mask, pos_embed, refpoint_embed, orig_res):
+                def custom_forward(*inputs):
+                    inputs = module(*inputs, memory_key_padding_mask=mask, pos=pos_embed, refpoints_unsigmoid=refpoint_embed, orig_res=orig_res)
+                    return inputs
+                return custom_forward
+            hs, cls_hs, references = checkpoint.checkpoint(custom_decoder(self.decoder, mask, pos_embed, refpoint_embed, (h,w)), tgt, memory)
+        else:
+            hs, cls_hs, references = self.decoder(tgt, memory, memory_key_padding_mask=mask,
+                                        pos=pos_embed, refpoints_unsigmoid=refpoint_embed, orig_res=(h,w))
+            
         return hs, cls_hs, references
 
 
@@ -578,6 +600,7 @@ def build_transformer(cfg):
         activation="relu",
         num_patterns=cfg.CONFIG.MODEL.NUM_PATTERNS,
         bbox_embed_diff_each_layer=cfg.CONFIG.MODEL.BBOX_EMBED_DIFF_EACH_LAYER,
+        gradient_checkpointing=cfg.CONFIG.GRADIENT_CHECKPOINTING,                
     )
 
 

train_dab_conv_trans.py
@@ -39,7 +39,7 @@ def main_worker(cfg):
         print_log(save_path, "use single frame:", cfg.CONFIG.MODEL.SINGLE_FRAME)
     model, criterion, postprocessors = build_model(cfg)
     model = deploy_model(model, cfg, is_tuber=True)
-    model = torch.compile(model)
+    # model = torch.compile(model)
     num_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     if cfg.DDP_CONFIG.GPU_WORLD_RANK == 0:    
         print_log(save_path, 'Number of parameters in the model: %6.2fM' % (num_parameters / 1000000))
@@ -142,6 +142,7 @@ if __name__ == '__main__':
     parser.add_argument('--random_seed', default=1, type=int, help='random_seed')
     parser.add_argument('--debug', action='store_true', help="debug, and ddp is disabled")
     parser.add_argument('--eff', action='store_true', help="only for AVA, efficiently output only keyframe")
+    parser.add_argument('--grad_ckpt', action='store_true', help="use gradient checkpoint")
     args = parser.parse_args()
     random.seed(args.random_seed)
     np.random.seed(args.random_seed)
@@ -166,7 +167,9 @@ if __name__ == '__main__':
         cfg.CONFIG.EFFICIENT = True
     else:
         cfg.CONFIG.EFFICIENT = False
-    
+    if args.grad_ckpt:
+        cfg.CONFIG.GRADIENT_CHECKPOINTING = True
+
     import socket 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))