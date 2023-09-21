debug: fix for gradient checkpointing, logging, and modeling

models/backbones/ir_CSN_152.py
@@ -11,6 +11,9 @@ import math
 from functools import partial
 import scipy.io as sio
 from torchvision.models.video.resnet import VideoResNet
+from utils.utils import print_log
+import os
+import torch.utils.checkpoint as checkpoint
 
 eps = 1e-3
 bn_mmt = 0.1
@@ -98,9 +101,12 @@ class ResNeXt(nn.Module):
                  block_nums,
                  num_classes=400,
                  use_affine=True,
-                 last_stride=True):
+                 last_stride=True,
+                 gradient_checkpointing=False,
+                 ):
 
         self.use_affine = use_affine
+        self.gradient_checkpointing = gradient_checkpointing
         self.in_planes = 64
         self.num_classes = num_classes
 
@@ -133,7 +139,7 @@ class ResNeXt(nn.Module):
         last_stride = 2 if last_stride else 1
         self.layer4 = self._make_layer(block, in_planes=1024, planes=512, blocks=block_nums[3],
                                        stride=last_stride, temporal_stride=2, expansion=4)
-        print("last stride: {}".format(last_stride))
+        # print("last stride: {}".format(last_stride))
 
         self.avgpool = nn.AdaptiveAvgPool3d(output_size=(1, 1, 1))
 
@@ -180,8 +186,17 @@ class ResNeXt(nn.Module):
 
         x = self.layer1(x)
         x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.layer3[0](x)
+        if self.gradient_checkpointing:
+            # def custom_layer(module):
+                # def custom_forward(*inputs):
+                    # return module(*inputs)
+                # return custom_forward
+            x = checkpoint.checkpoint_sequential(self.layer3[1:], len(self.layer3[1:]), x)
+            x = checkpoint.checkpoint_sequential(self.layer4, len(self.layer4), x)
+        else:
+            x = self.layer3[1:](x)
+            x = self.layer4(x)
 
         return x, None
 
@@ -194,7 +209,10 @@ def build_model(n_classes=400,
                 load_pretrain=True,
                 use_affine=True,
                 tune_point=5,
-                last_stride=True):
+                last_stride=True,
+                gpu_world_rank=0,
+                log_path=None,
+                gradient_checkpointing=False):
 
     model = ResNeXt(ResNeXtBottleneck,
                     num_classes=n_classes,
@@ -202,10 +220,12 @@ def build_model(n_classes=400,
                     block_nums=[3, 8, 36, 3],
                     sample_duration=sample_duration,
                     use_affine=use_affine,
-                    last_stride=last_stride)
+                    last_stride=last_stride,
+                    gradient_checkpointing=gradient_checkpointing,
+                    )
 
     if load_pretrain:
-        load_weights(model, pretrain_path=pretrain_path, load_fc=False, use_affine=use_affine, tune_point=tune_point)
+        load_weights(model, pretrain_path=pretrain_path, load_fc=False, use_affine=use_affine, tune_point=tune_point, gpu_world_rank=gpu_world_rank, log_path=log_path)
 
     return model
 
@@ -239,9 +259,10 @@ def copy_bn(layer, layer_name, weights, weights_dict_copy, use_affine=True):
                      weights_dict_copy=weights_dict_copy)
 
 
-def load_weights(model, pretrain_path, load_fc=True, use_affine=False, tune_point=5):
+def load_weights(model, pretrain_path, load_fc=True, use_affine=False, tune_point=5, gpu_world_rank=0, log_path=None):
     import scipy.io as sio
-    print("load weights plus")
+    if gpu_world_rank == 0:
+        print_log(log_path, "load weights plus")    
     r_weights = sio.loadmat(pretrain_path)
     r_weights_copy = sio.loadmat(pretrain_path)
 
@@ -311,15 +332,17 @@ def load_weights(model, pretrain_path, load_fc=True, use_affine=False, tune_poin
                      weights=r_weights['last_out_L400_b'][0],
                      weights_dict_copy=r_weights_copy)
 
-    print("load pretrain model from " + pretrain_path)
-    print("load fc", load_fc)
+    if gpu_world_rank == 0:
+        print_log(log_path, "load pretrain model from " + pretrain_path)
+        print_log(log_path, "load fc", load_fc)
     for k, v in r_weights_copy.items():
         if "momentum" not in k and "model_iter" not in k and "__globals__" not in k and "__header__" not in k and "lr" not in k and "__version__" not in k:
-            print(k, v.shape)
+            if gpu_world_rank == 0: print_log(log_path, k, v.shape)
 
 
 def build_CSN(cfg):
     tune_point = 4
+    log_path = os.path.join(cfg.CONFIG.LOG.BASE_PATH, cfg.CONFIG.LOG.EXP_NAME)
     model = build_model(n_classes=cfg.CONFIG.DATA.NUM_CLASSES,
                         sample_size=cfg.CONFIG.DATA.IMG_SIZE,
                         sample_duration=cfg.CONFIG.MODEL.TEMP_LEN,
@@ -328,8 +351,11 @@ def build_CSN(cfg):
                         load_pretrain=cfg.CONFIG.MODEL.PRETRAINED,
                         use_affine=False,
                         tune_point=tune_point,
-                        last_stride=cfg.CONFIG.MODEL.LAST_STRIDE)
-    print("build CSN-152, tune point: {}".format(tune_point))
+                        last_stride=cfg.CONFIG.MODEL.LAST_STRIDE,
+                        log_path=log_path,
+                        gradient_checkpointing=cfg.CONFIG.GRADIENT_CHECKPOINTING,)
+    if cfg.DDP_CONFIG.GPU_WORLD_RANK == 0:
+        print_log(log_path, "build CSN-152, tune point: {}".format(tune_point))
     return model
 
 

models/dab_conv_trans_detr/dab_transformer.py
@@ -127,7 +127,7 @@ class Transformer(nn.Module):
                                                 dropout, activation, normalize_before, keep_query_pos=keep_query_pos)
         cls_decoder_layer = TransformerClassDecoderLayer(d_model, nhead, dim_feedforward, dropout, activation, num_conv_blocks)        
         decoder_norm = nn.LayerNorm(d_model)
-        self.decoder = TransformerDecoder(decoder_layer, num_decoder_layers, decoder_norm,
+        self.decoder = TransformerDecoder(decoder_layer, cls_decoder_layer, num_decoder_layers, decoder_norm,
                                           return_intermediate=return_intermediate_dec,
                                           d_model=d_model, query_dim=query_dim, keep_query_pos=keep_query_pos, query_scale_type=query_scale_type,
                                           modulate_hw_attn=modulate_hw_attn,
@@ -623,7 +623,7 @@ class TransformerDecoder(nn.Module):
                     def custom_forward2(*inputs):
                         return module(*inputs)
                     return custom_forward2
-                output, actor_feature = checkpoint.checkpoint(custom_layer(layer),
+                output, actor_feature, q_memory = checkpoint.checkpoint(custom_layer(layer),
                                                output,
                                                memory,
                                                tgt_mask,
@@ -643,14 +643,14 @@ class TransformerDecoder(nn.Module):
                                                    orig_res,
                                                    len(tgt))
             else:
-                output, actor_feature = layer(output, memory, tgt_mask=tgt_mask,
+                output, actor_feature, q_memory = layer(output, memory, tgt_mask=tgt_mask,
                             memory_mask=memory_mask,
                             tgt_key_padding_mask=tgt_key_padding_mask,
                             memory_key_padding_mask=memory_key_padding_mask,
                             pos=pos, query_pos=query_pos, query_sine_embed=query_sine_embed,
                             is_first=(layer_id == 0))
 
-                cls_output = cls_layer(actor_feature.clone().detach(), memory, pos, query_sine_embed, self.class_queries, orig_res, len(tgt))
+                cls_output = cls_layer(actor_feature.clone().detach(), q_memory, pos[0], query_sine_embed, self.class_queries, orig_res, len(tgt))
             
             if layer_id != 0:
                 cls_output = self.cls_norm(cls_output + prev_output)
@@ -949,7 +949,7 @@ class TransformerClassDecoderLayer(nn.Module):
         self.dropout2_ = nn.Dropout(dropout) # self.dropout_
         self.cls_norm_ = nn.LayerNorm(d_model)
 
-    def forward(self, actor_feature, memory, pos, query_sine_embed, class_queries, orig_res, num_queries):
+    def forward(self, actor_feature, q_memory, pos, query_sine_embed, class_queries, orig_res, num_queries):
                     
         # separate classification branch from localization
         actor_feature2 = self.cls_linear2(self.dropout1(self.activation(self.cls_linear1(actor_feature))))
@@ -958,8 +958,11 @@ class TransformerClassDecoderLayer(nn.Module):
 
         # apply actor-centric convolution
         h, w = orig_res
-        actor_feature_expanded = actor_feature.flatten(0,1)[..., None, None].expand(-1, -1, h, w) # N_q*B, D, H, W
-        encoded_feature_expanded = memory[:, None].expand(-1, num_queries, -1, -1).flatten(1,2).view(h,w,-1,actor_feature.shape[-1]).permute(2,3,0,1) # N_q*B, D, H, W
+        actor_feature_expanded = actor_feature.flatten(0,1)[..., None, None].expand(-1, -1, h, w) # N_q*B*t, D, H, W
+        # memory needs to be hw, bs*t, d
+        # q_memory: n_q, hw, bs*t, d
+        # encoded_feature_expanded = q_memory.transpose(0,1).flatten(1,2).view(h,w,-1,actor_feature.shape[-1]).permute(2,3,0,1) # N_q*B(*t), D, H, W
+        encoded_feature_expanded = rearrange(q_memory, "N (H W) BT D -> (N BT) D H W", H=h, W=w)
         cls_feature = actor_feature_expanded + encoded_feature_expanded
         cls_feature = self.conv_norm(cls_feature.transpose(-1, 1).contiguous()).transpose(-1, 1).contiguous()            
         for block in self.conv_blocks:    
@@ -979,7 +982,7 @@ class TransformerClassDecoderLayer(nn.Module):
         query = query + self.dropout1(query2)
         query = self.norm1(query)
         
-        # class decoder cross-attention
+
         key = torch.cat([self.k_proj(cls_feature).flatten(2).permute(2,0,1), pos[:,None].expand(-1, num_queries, -1, -1).flatten(1,2)], dim=-1)
         cls_query_pos = self.cls_qpos_sine_proj(query_sine_embed).flatten(0,1)[None].expand(len(class_queries), -1 ,-1)
         query = torch.cat([query, cls_query_pos], dim=-1)