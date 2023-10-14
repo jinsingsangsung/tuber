365bc256b6fbf44812cf9929794be14bcfea8631
jinsung <jinsunglee96@gmail.com>
2023-10-14 17:29:52 +0900

    cuda file edited

--- ops/src/cuda/ms_deform_im2col_cuda_t.cuh
+++ ops/src/cuda/ms_deform_im2col_cuda_t.cuh
@@ -159,9 +159,9 @@ __device__ void ms_deform_attn_col2im_bilinear(const scalar_t* &bottom_data,
   {
     const int ptr1 = t_low_ptr_offset + h_low_ptr_offset + w_low_ptr_offset + base_ptr;
     v1 = bottom_data[ptr1];
-    grad_t_weight -= (hh + hw) * v1;
-    grad_h_weight -= (hw + ht) * v1;
-    grad_w_weight -= (hh + ht) * v1;
+    grad_t_weight -= (hh * hw) * v1;
+    grad_h_weight -= (hw * ht) * v1;
+    grad_w_weight -= (hh * ht) * v1;
     atomicAdd(grad_value+ptr1, w1*top_grad_value);
   }
   scalar_t v2 = 0;
@@ -169,9 +169,9 @@ __device__ void ms_deform_attn_col2im_bilinear(const scalar_t* &bottom_data,
   {
     const int ptr2 = t_low_ptr_offset + h_low_ptr_offset + w_high_ptr_offset + base_ptr;
     v2 = bottom_data[ptr2];
-    grad_t_weight -= (hh + lw) * v2;
-    grad_h_weight -= (ht + lw) * v2;
-    grad_w_weight += (ht + hh) * v2;
+    grad_t_weight -= (hh * lw) * v2;
+    grad_h_weight -= (ht * lw) * v2;
+    grad_w_weight += (ht * hh) * v2;
     atomicAdd(grad_value+ptr2, w2*top_grad_value);
   }
   scalar_t v3 = 0;
@@ -179,9 +179,9 @@ __device__ void ms_deform_attn_col2im_bilinear(const scalar_t* &bottom_data,
   {
     const int ptr3 = t_low_ptr_offset + h_high_ptr_offset + w_low_ptr_offset + base_ptr;
     v3 = bottom_data[ptr3];
-    grad_t_weight -= (lh + hw) * v3;
-    grad_h_weight += (ht + hw) * v3;
-    grad_w_weight -= (ht + lh) * v3;
+    grad_t_weight -= (lh * hw) * v3;
+    grad_h_weight += (ht * hw) * v3;
+    grad_w_weight -= (ht * lh) * v3;
     atomicAdd(grad_value+ptr3, w3*top_grad_value); 
   }
   scalar_t v4 = 0;
@@ -189,9 +189,9 @@ __device__ void ms_deform_attn_col2im_bilinear(const scalar_t* &bottom_data,
   {
     const int ptr4 = t_low_ptr_offset + h_high_ptr_offset + w_high_ptr_offset + base_ptr;
     v4 = bottom_data[ptr4];
-    grad_t_weight -= (lh + lw) * v4;
-    grad_h_weight += (ht + lw) * v4;
-    grad_w_weight += (ht + lh) * v4;
+    grad_t_weight -= (lh * lw) * v4;
+    grad_h_weight += (ht * lw) * v4;
+    grad_w_weight += (ht * lh) * v4;
     atomicAdd(grad_value+ptr4, w4*top_grad_value);
   }
   scalar_t v5 = 0;
@@ -199,9 +199,9 @@ __device__ void ms_deform_attn_col2im_bilinear(const scalar_t* &bottom_data,
   {
     const int ptr5 = t_high_ptr_offset + h_low_ptr_offset + w_low_ptr_offset + base_ptr;
     v1 = bottom_data[ptr5];
-    grad_t_weight += (hh + hw) * v5;
-    grad_h_weight -= (lt + hw) * v5;
-    grad_w_weight -= (lt + hh) * v5;
+    grad_t_weight += (hh * hw) * v5;
+    grad_h_weight -= (lt * hw) * v5;
+    grad_w_weight -= (lt * hh) * v5;
     atomicAdd(grad_value+ptr5, w1*top_grad_value);
   }
   scalar_t v6 = 0;
@@ -209,9 +209,9 @@ __device__ void ms_deform_attn_col2im_bilinear(const scalar_t* &bottom_data,
   {
     const int ptr6 = t_high_ptr_offset + h_low_ptr_offset + w_high_ptr_offset + base_ptr;
     v2 = bottom_data[ptr6];
-    grad_t_weight += (hh + lw) * v6;
-    grad_h_weight -= (lt + lw) * v6;
-    grad_w_weight += (lt + hh) * v6;
+    grad_t_weight += (hh * lw) * v6;
+    grad_h_weight -= (lt * lw) * v6;
+    grad_w_weight += (lt * hh) * v6;
     atomicAdd(grad_value+ptr6, w2*top_grad_value);
   }
   scalar_t v7 = 0;
@@ -219,9 +219,9 @@ __device__ void ms_deform_attn_col2im_bilinear(const scalar_t* &bottom_data,
   {
     const int ptr7 = t_high_ptr_offset + h_high_ptr_offset + w_low_ptr_offset + base_ptr;
     v3 = bottom_data[ptr7];
-    grad_t_weight += (lh + hw) * v7;
-    grad_h_weight += (lt + hw) * v7;
-    grad_w_weight -= (lt + lh) * v7;
+    grad_t_weight += (lh * hw) * v7;
+    grad_h_weight += (lt * hw) * v7;
+    grad_w_weight -= (lt * lh) * v7;
     atomicAdd(grad_value+ptr7, w3*top_grad_value); 
   }
   scalar_t v8 = 0;
@@ -229,9 +229,9 @@ __device__ void ms_deform_attn_col2im_bilinear(const scalar_t* &bottom_data,
   {
     const int ptr8 = t_high_ptr_offset + h_high_ptr_offset + w_high_ptr_offset + base_ptr;
     v8 = bottom_data[ptr8];
-    grad_t_weight += (lh + lw) * v8;    
-    grad_h_weight += (lt + lw) * v8;
-    grad_w_weight += (lt + lh) * v8;
+    grad_t_weight += (lh * lw) * v8;    
+    grad_h_weight += (lt * lw) * v8;
+    grad_w_weight += (lt * lh) * v8;
     atomicAdd(grad_value+ptr8, w4*top_grad_value);
   }
 
@@ -287,9 +287,9 @@ __device__ void ms_deform_attn_col2im_bilinear_gm(const scalar_t* &bottom_data,
   {
     const int ptr1 = t_low_ptr_offset + h_low_ptr_offset + w_low_ptr_offset + base_ptr;
     v1 = bottom_data[ptr1];
-    grad_t_weight -= (hh + hw) * v1;
-    grad_h_weight -= (hw + ht) * v1;
-    grad_w_weight -= (hh + ht) * v1;
+    grad_t_weight -= (hh * hw) * v1;
+    grad_h_weight -= (hw * ht) * v1;
+    grad_w_weight -= (hh * ht) * v1;
     atomicAdd(grad_value+ptr1, w1*top_grad_value);
   }
   scalar_t v2 = 0;
@@ -297,9 +297,9 @@ __device__ void ms_deform_attn_col2im_bilinear_gm(const scalar_t* &bottom_data,
   {
     const int ptr2 = t_low_ptr_offset + h_low_ptr_offset + w_high_ptr_offset + base_ptr;
     v2 = bottom_data[ptr2];
-    grad_t_weight -= (hh + lw) * v2;
-    grad_h_weight -= (ht + lw) * v2;
-    grad_w_weight += (ht + hh) * v2;
+    grad_t_weight -= (hh * lw) * v2;
+    grad_h_weight -= (ht * lw) * v2;
+    grad_w_weight += (ht * hh) * v2;
     atomicAdd(grad_value+ptr2, w2*top_grad_value);
   }
   scalar_t v3 = 0;
@@ -307,9 +307,9 @@ __device__ void ms_deform_attn_col2im_bilinear_gm(const scalar_t* &bottom_data,
   {
     const int ptr3 = t_low_ptr_offset + h_high_ptr_offset + w_low_ptr_offset + base_ptr;
     v3 = bottom_data[ptr3];
-    grad_t_weight -= (lh + hw) * v3;
-    grad_h_weight += (ht + hw) * v3;
-    grad_w_weight -= (ht + lh) * v3;
+    grad_t_weight -= (lh * hw) * v3;
+    grad_h_weight += (ht * hw) * v3;
+    grad_w_weight -= (ht * lh) * v3;
     atomicAdd(grad_value+ptr3, w3*top_grad_value); 
   }
   scalar_t v4 = 0;
@@ -317,9 +317,9 @@ __device__ void ms_deform_attn_col2im_bilinear_gm(const scalar_t* &bottom_data,
   {
     const int ptr4 = t_low_ptr_offset + h_high_ptr_offset + w_high_ptr_offset + base_ptr;
     v4 = bottom_data[ptr4];
-    grad_t_weight -= (lh + lw) * v4;
-    grad_h_weight += (ht + lw) * v4;
-    grad_w_weight += (ht + lh) * v4;
+    grad_t_weight -= (lh * lw) * v4;
+    grad_h_weight += (ht * lw) * v4;
+    grad_w_weight += (ht * lh) * v4;
     atomicAdd(grad_value+ptr4, w4*top_grad_value);
   }
   scalar_t v5 = 0;
@@ -327,9 +327,9 @@ __device__ void ms_deform_attn_col2im_bilinear_gm(const scalar_t* &bottom_data,
   {
     const int ptr5 = t_high_ptr_offset + h_low_ptr_offset + w_low_ptr_offset + base_ptr;
     v1 = bottom_data[ptr5];
-    grad_t_weight += (hh + hw) * v5;
-    grad_h_weight -= (lt + hw) * v5;
-    grad_w_weight -= (lt + hh) * v5;
+    grad_t_weight += (hh * hw) * v5;
+    grad_h_weight -= (lt * hw) * v5;
+    grad_w_weight -= (lt * hh) * v5;
     atomicAdd(grad_value+ptr5, w5*top_grad_value);
   }
   scalar_t v6 = 0;
@@ -337,9 +337,9 @@ __device__ void ms_deform_attn_col2im_bilinear_gm(const scalar_t* &bottom_data,
   {
     const int ptr6 = t_high_ptr_offset + h_low_ptr_offset + w_high_ptr_offset + base_ptr;
     v2 = bottom_data[ptr6];
-    grad_t_weight += (hh + lw) * v6;
-    grad_h_weight -= (lt + lw) * v6;
-    grad_w_weight += (lt + hh) * v6;
+    grad_t_weight += (hh * lw) * v6;
+    grad_h_weight -= (lt * lw) * v6;
+    grad_w_weight += (lt * hh) * v6;
     atomicAdd(grad_value+ptr6, w6*top_grad_value);
   }
   scalar_t v7 = 0;
@@ -347,9 +347,9 @@ __device__ void ms_deform_attn_col2im_bilinear_gm(const scalar_t* &bottom_data,
   {
     const int ptr7 = t_high_ptr_offset + h_high_ptr_offset + w_low_ptr_offset + base_ptr;
     v3 = bottom_data[ptr7];
-    grad_t_weight += (lh + hw) * v7;
-    grad_h_weight += (lt + hw) * v7;
-    grad_w_weight -= (lt + lh) * v7;
+    grad_t_weight += (lh * hw) * v7;
+    grad_h_weight += (lt * hw) * v7;
+    grad_w_weight -= (lt * lh) * v7;
     atomicAdd(grad_value+ptr7, w7*top_grad_value); 
   }
   scalar_t v8 = 0;
@@ -357,9 +357,9 @@ __device__ void ms_deform_attn_col2im_bilinear_gm(const scalar_t* &bottom_data,
   {
     const int ptr8 = t_high_ptr_offset + h_high_ptr_offset + w_high_ptr_offset + base_ptr;
     v8 = bottom_data[ptr8];
-    grad_t_weight += (lh + lw) * v8;    
-    grad_h_weight += (lt + lw) * v8;
-    grad_w_weight += (lt + lh) * v8;
+    grad_t_weight += (lh * lw) * v8;    
+    grad_h_weight += (lt * lw) * v8;
+    grad_w_weight += (lt * lh) * v8;
     atomicAdd(grad_value+ptr8, w8*top_grad_value);
   }
 
@@ -459,7 +459,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_blocksize_aware_reduce_v1(co
 {
   CUDA_KERNEL_LOOP(index, n)
   {
-    __shared__ scalar_t cache_grad_sampling_loc[blockSize * 2];
+    __shared__ scalar_t cache_grad_sampling_loc[blockSize * 3];
     __shared__ scalar_t cache_grad_attn_weight[blockSize];
     unsigned int tid = threadIdx.x;
     int _temp = index;
@@ -521,7 +521,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_blocksize_aware_reduce_v1(co
         if (tid == 0)
         {
           scalar_t _grad_w=cache_grad_sampling_loc[0], _grad_h=cache_grad_sampling_loc[1], _grad_T=cache_grad_sampling_loc[2], _grad_a=cache_grad_attn_weight[0];
-          int sid=2;
+          int sid=3;
           for (unsigned int tid = 1; tid < blockSize; ++tid)
           {
             _grad_w += cache_grad_sampling_loc[sid];
@@ -570,7 +570,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_blocksize_aware_reduce_v2(co
 {
   CUDA_KERNEL_LOOP(index, n)
   {
-    __shared__ scalar_t cache_grad_sampling_loc[blockSize * 2];
+    __shared__ scalar_t cache_grad_sampling_loc[blockSize * 3];
     __shared__ scalar_t cache_grad_attn_weight[blockSize];
     unsigned int tid = threadIdx.x;
     int _temp = index;
@@ -630,7 +630,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_blocksize_aware_reduce_v2(co
         
         __syncthreads();
 
-        for (unsigned int s=blockSize/2; s>0; s>>=1)
+        for (unsigned int s=blockSize/3; s>0; s/3)
         {
           if (tid < s) {
             const unsigned int xid1 = tid * 3;
@@ -685,7 +685,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v1(const int n,
   {
     extern __shared__ int _s[];
     scalar_t* cache_grad_sampling_loc = (scalar_t*)_s;
-    scalar_t* cache_grad_attn_weight = cache_grad_sampling_loc + 2 * blockDim.x;
+    scalar_t* cache_grad_attn_weight = cache_grad_sampling_loc + 3 * blockDim.x;
     unsigned int tid = threadIdx.x;
     int _temp = index;
     const int c_col = _temp % channels;
@@ -746,7 +746,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v1(const int n,
         if (tid == 0)
         {
           scalar_t _grad_w=cache_grad_sampling_loc[0], _grad_h=cache_grad_sampling_loc[1], _grad_T=cache_grad_sampling_loc[2], _grad_a=cache_grad_attn_weight[0];
-          int sid=2;
+          int sid=3;
           for (unsigned int tid = 1; tid < blockDim.x; ++tid)
           {
             _grad_w += cache_grad_sampling_loc[sid];
@@ -796,7 +796,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v2(const int n,
   {
     extern __shared__ int _s[];
     scalar_t* cache_grad_sampling_loc = (scalar_t*)_s;
-    scalar_t* cache_grad_attn_weight = cache_grad_sampling_loc + 2 * blockDim.x;
+    scalar_t* cache_grad_attn_weight = cache_grad_sampling_loc + 3 * blockDim.x;
     unsigned int tid = threadIdx.x;
     int _temp = index;
     const int c_col = _temp % channels;
@@ -811,9 +811,9 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v2(const int n,
     const scalar_t top_grad = grad_col[index];
 
     int data_weight_ptr = sampling_index * num_levels * num_point;
-    int data_loc_w_ptr = data_weight_ptr << 1;
+    int data_loc_w_ptr = data_weight_ptr * 3;
     const int grad_sampling_ptr = data_weight_ptr;
-    grad_sampling_loc += grad_sampling_ptr << 1;
+    grad_sampling_loc += grad_sampling_ptr * 3;
     grad_attn_weight += grad_sampling_ptr;
     const int grad_weight_stride = 1;
     const int grad_loc_stride = 3;
@@ -823,7 +823,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v2(const int n,
     for (int l_col=0; l_col < num_levels; ++l_col)
     {
       const int level_start_id = data_level_start_index[l_col];
-      const int spatial_T_ptr = l_col << 1;
+      const int spatial_T_ptr = l_col * 3;
       const int spatial_T = data_spatial_shapes[spatial_T_ptr];
       const int spatial_h = data_spatial_shapes[spatial_T_ptr + 1];
       const int spatial_w = data_spatial_shapes[spatial_T_ptr + 2];
@@ -841,35 +841,35 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v2(const int n,
         const scalar_t T_im = loc_T * spatial_T - 0.5;
         const scalar_t h_im = loc_h * spatial_h - 0.5;
         const scalar_t w_im = loc_w * spatial_w - 0.5;
-        *(cache_grad_sampling_loc+(threadIdx.x << 1)) = 0;
-        *(cache_grad_sampling_loc+((threadIdx.x << 1) + 1)) = 0;
-        *(cache_grad_sampling_loc+((threadIdx.x << 1) + 2)) = 0;
+        *(cache_grad_sampling_loc+(threadIdx.x * 3)) = 0;
+        *(cache_grad_sampling_loc+((threadIdx.x * 3) + 1)) = 0;
+        *(cache_grad_sampling_loc+((threadIdx.x * 3) + 2)) = 0;
         *(cache_grad_attn_weight+threadIdx.x)=0;
         if (T_im > -1 && h_im > -1 && w_im > -1 && T_im < spatial_T && h_im < spatial_h && w_im < spatial_w)
         {
           ms_deform_attn_col2im_bilinear(
             data_value_ptr, spatial_T, spatial_h, spatial_w, num_heads, channels, T_im, h_im, w_im, m_col, c_col,
             top_grad, weight, grad_value_ptr, 
-            cache_grad_sampling_loc+(threadIdx.x << 1), cache_grad_attn_weight+threadIdx.x);
+            cache_grad_sampling_loc+(threadIdx.x * 3), cache_grad_attn_weight+threadIdx.x);
         }
         
         __syncthreads();
 
-        for (unsigned int s=blockDim.x/2, spre=blockDim.x; s>0; s>>=1, spre>>=1)
+        for (unsigned int s=blockDim.x/3, spre=blockDim.x; s>0; s/3, spre/3)
         {
           if (tid < s) {
-            const unsigned int xid1 = tid << 1;
-            const unsigned int xid2 = (tid + s) << 1;
+            const unsigned int xid1 = tid * 3;
+            const unsigned int xid2 = (tid + s) * 3;
             cache_grad_attn_weight[tid] += cache_grad_attn_weight[tid + s];
             cache_grad_sampling_loc[xid1] += cache_grad_sampling_loc[xid2];
             cache_grad_sampling_loc[xid1 + 1] += cache_grad_sampling_loc[xid2 + 1];
             cache_grad_sampling_loc[xid1 + 2] += cache_grad_sampling_loc[xid2 + 2];
-            if (tid + (s << 1) < spre)
+            if (tid + (s * 3) < spre)
             {
-              cache_grad_attn_weight[tid] += cache_grad_attn_weight[tid + (s << 1)];
-              cache_grad_sampling_loc[xid1] += cache_grad_sampling_loc[xid2 + (s << 1)];
-              cache_grad_sampling_loc[xid1 + 1] += cache_grad_sampling_loc[xid2 + 1 + (s << 1)];
-              cache_grad_sampling_loc[xid1 + 2] += cache_grad_sampling_loc[xid2 + 2 + (s << 1)];
+              cache_grad_attn_weight[tid] += cache_grad_attn_weight[tid + (s * 3)];
+              cache_grad_sampling_loc[xid1] += cache_grad_sampling_loc[xid2 + (s * 3)];
+              cache_grad_sampling_loc[xid1 + 1] += cache_grad_sampling_loc[xid2 + 1 + (s * 3)];
+              cache_grad_sampling_loc[xid1 + 2] += cache_grad_sampling_loc[xid2 + 2 + (s * 3)];
             } 
           }
           __syncthreads();
@@ -931,9 +931,9 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v2_multi_blocks(const
     const scalar_t top_grad = grad_col[index];
 
     int data_weight_ptr = sampling_index * num_levels * num_point;
-    int data_loc_w_ptr = data_weight_ptr << 1;
+    int data_loc_w_ptr = data_weight_ptr * 3;
     const int grad_sampling_ptr = data_weight_ptr;
-    grad_sampling_loc += grad_sampling_ptr << 1;
+    grad_sampling_loc += grad_sampling_ptr * 3;
     grad_attn_weight += grad_sampling_ptr;
     const int grad_weight_stride = 1;
     const int grad_loc_stride = 3;
@@ -943,7 +943,7 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v2_multi_blocks(const
     for (int l_col=0; l_col < num_levels; ++l_col)
     {
       const int level_start_id = data_level_start_index[l_col];
-      const int spatial_T_ptr = l_col << 1;
+      const int spatial_T_ptr = l_col * 3;
       const int spatial_T = data_spatial_shapes[spatial_T_ptr];
       const int spatial_h = data_spatial_shapes[spatial_T_ptr + 1];
       const int spatial_w = data_spatial_shapes[spatial_T_ptr + 2];
@@ -961,35 +961,35 @@ __global__ void ms_deformable_col2im_gpu_kernel_shm_reduce_v2_multi_blocks(const
         const scalar_t T_im = loc_T * spatial_T - 0.5;
         const scalar_t h_im = loc_h * spatial_h - 0.5;
         const scalar_t w_im = loc_w * spatial_w - 0.5;
-        *(cache_grad_sampling_loc+(threadIdx.x << 1)) = 0;
-        *(cache_grad_sampling_loc+((threadIdx.x << 1) + 1)) = 0;
-        *(cache_grad_sampling_loc+((threadIdx.x << 1) + 2)) = 0;
+        *(cache_grad_sampling_loc+(threadIdx.x * 3)) = 0;
+        *(cache_grad_sampling_loc+((threadIdx.x * 3) + 1)) = 0;
+        *(cache_grad_sampling_loc+((threadIdx.x * 3) + 2)) = 0;
         *(cache_grad_attn_weight+threadIdx.x)=0;
         if (T_im > -1 && h_im > -1 && w_im > -1 && T_im < spatial_T && h_im < spatial_h && w_im < spatial_w)
         {
           ms_deform_attn_col2im_bilinear(
             data_value_ptr, spatial_T, spatial_h, spatial_w, num_heads, channels, T_im, h_im, w_im, m_col, c_col,
             top_grad, weight, grad_value_ptr, 
-            cache_grad_sampling_loc+(threadIdx.x << 1), cache_grad_attn_weight+threadIdx.x);
+            cache_grad_sampling_loc+(threadIdx.x * 3), cache_grad_attn_weight+threadIdx.x);
         }
         
         __syncthreads();
 
-        for (unsigned int s=blockDim.x/2, spre=blockDim.x; s>0; s>>=1, spre>>=1)
+        for (unsigned int s=blockDim.x/3, spre=blockDim.x; s>0; s/3, spre/3)
         {
           if (tid < s) {
-            const unsigned int xid1 = tid << 1;
-            const unsigned int xid2 = (tid + s) << 1;
+            const unsigned int xid1 = tid * 3;
+            const unsigned int xid2 = (tid + s) * 3;
             cache_grad_attn_weight[tid] += cache_grad_attn_weight[tid + s];
             cache_grad_sampling_loc[xid1] += cache_grad_sampling_loc[xid2];
             cache_grad_sampling_loc[xid1 + 1] += cache_grad_sampling_loc[xid2 + 1];
             cache_grad_sampling_loc[xid1 + 2] += cache_grad_sampling_loc[xid2 + 2];
-            if (tid + (s << 1) < spre)
+            if (tid + (s * 3) < spre)
             {
-              cache_grad_attn_weight[tid] += cache_grad_attn_weight[tid + (s << 1)];
-              cache_grad_sampling_loc[xid1] += cache_grad_sampling_loc[xid2 + (s << 1)];
-              cache_grad_sampling_loc[xid1 + 1] += cache_grad_sampling_loc[xid2 + 1 + (s << 1)];
-              cache_grad_sampling_loc[xid1 + 2] += cache_grad_sampling_loc[xid2 + 2 + (s << 1)];
+              cache_grad_attn_weight[tid] += cache_grad_attn_weight[tid + (s * 3)];
+              cache_grad_sampling_loc[xid1] += cache_grad_sampling_loc[xid2 + (s * 3)];
+              cache_grad_sampling_loc[xid1 + 1] += cache_grad_sampling_loc[xid2 + 1 + (s * 3)];
+              cache_grad_sampling_loc[xid1 + 2] += cache_grad_sampling_loc[xid2 + 2 + (s * 3)];
             }
           }
           __syncthreads();
